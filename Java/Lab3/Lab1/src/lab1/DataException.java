bbd703454bfb5e2b443415588ef4b8341f460280
Danila2105 <max.sergunov2017@yandex.ru>
2025-03-11 18:49:47 +0300

    lab3

---
+++ Java/Lab3/Lab1/src/lab1/DataException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab1;
+
+/**
+ *
+ * @author student
+ */
+public class DataException extends Exception {
+    public DataException (String message){
+        super(message);
+    }
+}
